2ec3e0dc3d7f7abd0be304957d267b0ea3112b89
Moeen Basra <moeen.basra@smartly.io>
2023-07-14 15:02:50 +0200

    update the build

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,10 @@
 FROM php:8.2-fpm-alpine
 
-ARG user=forge
-ARG gid=1000
-ARG xdebug=true
-ARG xdebug_remote_host=docker.for.mac.localhost
-ARG xdebug_port=9001
-
-# Set Build Environment Variables
-ENV USER=$user
-ENV XDEBUG=$xdebug
-ENV XDEBUG_REMOTE_HOST=$xdebug_remote_host
-ENV XDEBUG_PORT=$xdebug_port
-
-# ADD and set Group
-RUN addgroup -g $gid $USER && adduser -G $USER -g $USER -s /bin/sh -D $USER
+ARG xdebug=${xdebug:-false}
+ARG xdebug_remote_host=${xdebug_remote_host:-host.docker.internal}
+ARG xdebug_port=${xdebug_port:-9000}
 
 # Install PHP extensions
 ADD ./install-php.sh /usr/sbin/install-php.sh
 RUN chmod +x /usr/sbin/install-php.sh \
-    && /usr/sbin/install-php.sh
+    && /usr/sbin/install-php.sh --xdebug=$xdebug --xdebug-remote-host=$xdebug_remote_host --xdebug-port=$xdebug_port
--- install-php.sh
+++ install-php.sh
@@ -1,18 +1,61 @@
 #!/bin/sh
 
-apk add --update --no-cache $PHPIZE_DEPS \
-    bzip2-dev freetype-dev icu-dev jpeg-dev libjpeg-turbo-dev libmemcached-dev libpng-dev libxml2-dev libzip-dev openldap-dev postgresql-dev \
+# Parse script arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --xdebug=*)
+        xdebug="${1#*=}"
+        ;;
+    --xdebug-remote-host=*)
+        xdebug_remote_host="${1#*=}"
+        ;;
+    --xdebug-port=*)
+        xdebug_port="${1#*=}"
+        ;;
+    *)
+        echo "Error: Invalid argument: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+# Check if PHPIZE_DEPS is set
+if [ -z "$PHPIZE_DEPS" ]; then
+    echo "Error: PHPIZE_DEPS variable is not set."
+    exit 1
+fi
+
+# Update package repositories
+apk update
+
+# Install required packages
+apk add --update --no-cache \
+    $PHPIZE_DEPS \
+    bzip2-dev \
+    freetype-dev \
+    icu-dev \
+    jpeg-dev \
+    libjpeg-turbo-dev \
+    libmemcached-dev \
+    libpng-dev \
+    libxml2-dev \
+    libzip-dev \
+    openldap-dev \
+    postgresql-dev \
     freetype \
     libpng \
     libjpeg-turbo \
-    zip
+    zip \
+    linux-headers
 
+# Configure and install gd extension
+docker-php-ext-configure gd --with-freetype --with-jpeg
+NPROC=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)
+docker-php-ext-install -j${NPROC} gd
 
-docker-php-ext-configure gd --with-freetype --with-jpeg \
-    && NPROC=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1) \
-    && docker-php-ext-install -j${NPROC} gd \
-    && docker-php-ext-install bcmath bz2 intl ldap pcntl pdo_mysql pdo_pgsql soap sockets xml zip
-
+# Install other required extensions
+docker-php-ext-install bcmath bz2 intl ldap pcntl pdo_mysql pdo_pgsql soap sockets xml zip
 
 # Install php pickle replacement for pecl
 curl -sSLo /usr/local/bin/pickle https://github.com/FriendsOfPHP/pickle/releases/latest/download/pickle.phar
@@ -21,31 +64,29 @@ pickle install -n --defaults igbinary
 pickle install -n --defaults memcached
 pickle install -n --defaults redis
 pickle install -n --defaults xmlrpc
+pickle install -n --defaults xdebug
+docker-php-ext-enable igbinary memcached redis xmlrpc >/dev/null
 
-docker-php-ext-enable igbinary memcached redis xmlrpc > /dev/null
-
-sed -i "s/user = www-data/user = ${USER}/g" /usr/local/etc/php-fpm.d/www.conf
-sed -i "s/group = www-data/group = ${USER}/g" /usr/local/etc/php-fpm.d/www.conf
-
-if [ $XDEBUG ]
-then
-    pickle install -n --defaults xdebug
-    docker-php-ext-enable xdebug > /dev/null
-    echo "remote_host=${XDEBUG_REMOTE_HOST}
-remote_port=${XDEBUG_PORT}
+# Install Xdebug if enabled
+if [ "$xdebug" = true ]; then
+    docker-php-ext-enable xdebug >/dev/null
+    echo "remote_host=${xdebug_remote_host}
+remote_port=${xdebug_port}
 remote_enable=1
 idekey=IDE_DEBUG
 error_reporting=E_ALL
 display_startup_errors=On
-display_errors=On" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+display_errors=On" >>/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
-# Install composer
+# Install Composer
 curl --silent --show-error https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-# Install php unit
+
+# Install PHPUnit
 curl -sSL -o /usr/bin/phpunit https://phar.phpunit.de/phpunit.phar && chmod +x /usr/bin/phpunit
 
-apk del $TMP
+# Remove temporary packages
+apk del $PHPIZE_DEPS
 
-# # Set timezone
-# #RUN echo Asia/Karachi > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
+# Set timezone (optional)
+# echo "Asia/Karachi" > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
